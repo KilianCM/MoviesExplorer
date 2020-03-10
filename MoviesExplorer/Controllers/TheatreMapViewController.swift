8dc44d93c552aea96eb2e0e183538f811cd38c86
kchamiot <chamiot.k@gmail.com>
2020-03-10 16:01:18 +0100

    Open action sheet menu to open movieTheater URL

--- MoviesExplorer/Controllers/TheatreMapViewController.swift
+++ MoviesExplorer/Controllers/TheatreMapViewController.swift
@@ -9,20 +9,35 @@
 import UIKit
 import MapKit
 
-class TheatreMapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class TheatreMapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     private var locationManager = CLLocationManager()
+    private var mapItems: [MKMapItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         mapView.showsUserLocation = true
-        
-        // show only movie theater on map
-        mapView.pointOfInterestFilter = MKPointOfInterestFilter(including: [MKPointOfInterestCategory.movieTheater])
-
+        mapView.pointOfInterestFilter = MKPointOfInterestFilter(excluding: [.movieTheater])
+        zoomOnUserLocation()
+        loadPointOfInterest { mapItems in
+            self.mapItems = mapItems
+            let annotations = mapItems.compactMap { mapItem -> MKAnnotation? in
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                annotation.subtitle = mapItem.url?.absoluteString
+                return annotation
+            }
+            DispatchQueue.main.async {
+                self.mapView.addAnnotations(annotations)
+            }
+        }
+    }
+    
+    private func zoomOnUserLocation() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
 
@@ -41,25 +56,12 @@ class TheatreMapViewController: UIViewController, MKMapViewDelegate, CLLocationM
         DispatchQueue.main.async {
             self.locationManager.startUpdatingLocation()
         }
-        
-        loadPointOfInterest { mapItems in
-            let annotations = mapItems.compactMap { mapItem -> MKAnnotation? in
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = mapItem.placemark.coordinate
-                annotation.title = mapItem.name
-                annotation.subtitle = mapItem.url?.absoluteString
-                return annotation
-            }
-            DispatchQueue.main.async {
-                self.mapView.addAnnotations(annotations)
-            }
-        }
     }
     
     private func loadPointOfInterest(completion: @escaping (([MKMapItem]) -> Void)) {
         let request = MKLocalSearch.Request()
         request.resultTypes = MKLocalSearch.ResultType.pointOfInterest
-        request.pointOfInterestFilter = MKPointOfInterestFilter(including: [MKPointOfInterestCategory.movieTheater])
+        request.pointOfInterestFilter = MKPointOfInterestFilter(including: [MKPointOfInterestCategory.movieTheater, .theater])
         request.region = mapView.region
         
         let search = MKLocalSearch(request: request)
@@ -72,8 +74,34 @@ class TheatreMapViewController: UIViewController, MKMapViewDelegate, CLLocationM
         }
     }
     
+    private func displayActionSheet(url: String?) {
+        guard let urlAsString = url, let url = URL(string: urlAsString) else {
+            return
+        }
+        
+        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let deleteAction = UIAlertAction(title: "Ouvrir le site web", style: .default, handler: { (UIAlertAction) in
+            UIApplication.shared.open(url)
+        })
+        let cancelAction = UIAlertAction(title: "Annuler", style: .cancel)
+            
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(cancelAction)
+            
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+}
+
+extension TheatreMapViewController: MKMapViewDelegate {
+    
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView)
     {
-        //TODO: handle click on annotation
+        if let url = view.annotation?.subtitle {
+            displayActionSheet(url: url)
+        }
+    }
+
+    func showAnnotationDisclosure(sender: AnyObject) {
+        print("Disclosure button clicked")
     }
 }
